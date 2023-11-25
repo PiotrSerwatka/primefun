c06ae2e5c1981198ad869602f10ce5c7ce311666
pionier <pionierprog@interia.pl>
2023-11-25 12:46:33 +0100

    Initial commit

--- src/test/java/com/project/one/firstprojectonmac/FirstProjectOnMacApplicationTests.java
+++ src/test/java/com/project/one/firstprojectonmac/FirstProjectOnMacApplicationTests.java
@@ -1,12 +1,12 @@
 package com.project.one.firstprojectonmac;
 
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.project.one.firstprojectonmac.service.PrimeService;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class FirstProjectOnMacApplicationTests {
@@ -16,27 +16,24 @@ public class FirstProjectOnMacApplicationTests {
 
 
 	@ParameterizedTest
-	@CsvSource(value = {"1:2", "2:3", "3:5", "5:11", "100:541", "10000:104729", "100000:1299709"}, delimiter = ':') //, "10000:104729", "100000:1299709", "1000000:15485863"}
+	@CsvSource(value = {"1:2", "2:3", "3:5", "5:11", "100:541", "100000:1299709"}, delimiter = ':') //, "10000:104729", "100000:1299709", "10000:104729", "100000:1299709", "1000000:15485863"}
 	void first_prime_number(int primeIndex, int expected) {
 		
-		int result = primeService.primeNumber(primeIndex);
+		long result = primeService.primeNumber(primeIndex);
 		
 		assertThat(result).isEqualTo(expected);
 
 	}
 
 	@ParameterizedTest
-	@CsvSource(value = {"2:2", "3:2,3", "5:2,3,5", "10:2,3,5,7"}, delimiter = ':') //, "10000:104729", "100000:1299709", "1000000:15485863"}
+	@CsvSource(value = {"2:1,2", "3:2,3", "5:3,5", "10:4,7", "1299709:100000,1299709"}, delimiter = ':') //, "10000:104729", "100000:1299709", "1000000:15485863"}
 	void prime_numbers_parallel(long primeNumbersToValue, String expectedResult) {
+
 		List<Long> listOfPrimeNumbers = primeService.calculatPrimeInRange(primeNumbersToValue);
 
-		assertThat(listOfPrimeNumbers).containsAll(
-				Arrays.stream(
-						expectedResult
-							.split(",")
-						).map(Long::valueOf)
-						.toList()
-		);
+		assertThat(format("%s,%s", listOfPrimeNumbers.size(),listOfPrimeNumbers.getLast()))
+				.isEqualTo(expectedResult);
+
 	}
 	
 }
--- src/test/java/com/project/one/firstprojectonmac/service/PrimeService.java
+++ src/test/java/com/project/one/firstprojectonmac/service/PrimeService.java
@@ -10,75 +10,111 @@ import java.util.List;
 @Service
 public class PrimeService {
 
-	public int primeNumber(int i) {
-		int result = 0;
-		if (i == 1) {
-			return 2;
-		}
-		if (i == 2) {
-			return 3;
-		}
-		int counter = 0;
-		int potentialPrimeNumver = 3;
-		while (counter != i-1){
-			LocalDateTime start = LocalDateTime.now();
-			result = potentialPrimeNumver;
-			boolean notAPrime = false;
-			for (int divider = 3 ; divider < potentialPrimeNumver / 2; divider++){
-				if (potentialPrimeNumver % divider == 0) {
-					notAPrime = true;
-					break;
-				}
-			}
-			if (!notAPrime) {
-				counter++;
-				if (counter %10000 == 0) {
-					System.out.printf("PrimeCount=%s out of %s at: %s and took: %s%n", counter, i, LocalDateTime.now(), Duration.between(start,LocalDateTime.now()));
-				}
-			}
-			potentialPrimeNumver+=2;
-		}
+    public long primeNumber(long i) {
+        long result = 0;
+        if (i == 1) {
+            return 2;
+        }
+        if (i == 2) {
+            return 3;
+        }
+        int counter = 0;
+        long potentialPrimeNumver = 3;
+        while (counter != i - 1) {
+            LocalDateTime start = LocalDateTime.now();
+            result = potentialPrimeNumver;
+            if (isAPrime(potentialPrimeNumver)) {
+                counter++;
+                if (counter % 10000 == 0) {
+                    System.out.printf("PrimeCount=%s out of %s at: %s and took: %s%n", counter, i, LocalDateTime.now(), Duration.between(start, LocalDateTime.now()));
+                }
+            }
+            potentialPrimeNumver += 2;
+        }
 
-		return result;
-	}
+        return result;
+    }
+
+    private boolean isAPrime(Long potentialPrimeNumver) {
+		if (potentialPrimeNumver == 1 || potentialPrimeNumver == 4 || potentialPrimeNumver % 2 == 0) return false;
+        for (int divider = 3; hasNextBatchToCalculate(potentialPrimeNumver / 2, divider); divider++) {
+            if (potentialPrimeNumver % divider == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
-	public List<Long> calculatPrimeInRange(long to) {
-
-		if (to == 2) {
-			return List.of(2L);
-		}
-		if (to == 3) {
-			return List.of(2L, 3L);
-		}
+    public List<Long> calculatPrimeInRange(long to) {
 
 		List<Long> result = new LinkedList<>();
 
-		List<Pair> batches = calculateBatches(to);
+        if (to >= 2) {
+            result.add(2L);
+        }
+
+        List<Pair> batches = calculateBatches(to);
+
+		result.addAll(batches.stream().parallel()
+				.map(this::findPrimesInRange)
+				.flatMap(List::stream)
+				.toList());
 
 
+        return result;
+    }
 
-		return result;
+	private List<Long> findPrimesInRange(Pair pair) {
+		List<Long> primeFound = new LinkedList<>();
+		for (long i = pair.from; i<= pair.to; i++) {
+			if (isAPrime(i)) {
+				primeFound.add(i);
+			}
+		}
+		return primeFound;
 	}
 
 	private List<Pair> calculateBatches(long to) {
-		List<Pair> result = new LinkedList<>();
-		int liczbaRdzeni = 2;//Runtime.getRuntime().availableProcessors();
+        List<Pair> result = new LinkedList<>();
+        long liczbaRdzeni = Runtime.getRuntime().availableProcessors();
 
-		long numberOfBatches = to/liczbaRdzeni;
-		long incrementBy = to / numberOfBatches;
-
-		for (long i = 1 ; i <= numberOfBatches; i+=incrementBy) {
-			//1-5, 5-10, 10+15
-            long end = i + incrementBy - 1;
-			Pair pair = new Pair(i, end);
-			result.add(pair);
+		if (liczbaRdzeni > to) {
+			liczbaRdzeni = to;
 		}
 
-		return result;
+        long numberOfBatches = to / liczbaRdzeni;
+        long incrementBy = to / numberOfBatches;
+
+		long i = 1;
+		boolean hasNextBatchToCalculate = hasNextBatchToCalculate(to, i);
+        while ( hasNextBatchToCalculate ) {
+            //1-5, 5-10, 10+15
+			long end = i + incrementBy - 1;
+			Pair pair = getPair(to, hasNextBatchToCalculate, i, end);
+			result.add(pair);
+
+			i += incrementBy;
+			hasNextBatchToCalculate = hasNextBatchToCalculate(to, i);
+        }
+
+        return result;
+    }
+
+	private static Pair getPair(long to, boolean hasNextBatchToCalculate, long i, long end) {
+		if (hasNextBatchToCalculate) {
+			 return new Pair(i, end);
+		} else {
+			 return new Pair(i, to);
+		}
+	}
+
+	private static boolean hasNextBatchToCalculate(long to, long i) {
+		return i < to;
 	}
 
 
-	record Pair(long from, long to){}
+	record Pair(long from, long to) {
+    }
 
 }
